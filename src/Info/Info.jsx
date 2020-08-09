a73c22ad11b836484a0dbae0c3b9bd8eaf6730fc
Tianchang WANG <wangtcalex@gmail.com>
2020-08-09 11:32:02 +0800

    update

--- src/Info/Info.jsx
+++ src/Info/Info.jsx
@@ -6,13 +6,35 @@ function Info()  {
     <div className='info'>
         <div className='header'>
             {/* <div className='header-img'> */}
-              <img className = 'header-img'
+              <img className='header-img'
                    src={require('../assets/header-logo.png')} 
                    alt=""/>
-            {/* </div> */}
             <div className='header-text'>
                 Maria Cai
             </div>
+        </div>
+
+        <div className='intro'>
+          <img className='intro-img' 
+               src={require('../assets/avatar-bw.jpeg')}
+               alt=""
+            />
+          <div className='descs'>
+            <div className='name'>Maria Cai</div>
+            <div className='name'>Artist</div>
+            <div className='contact'>
+            ⬛	E-mail: maria55712786@gmail.com</div>
+            <div className='contact'>
+             ⬛	 WeChat: c632407211</div>
+            <div className='contact'>
+            ⬛	WeChat Official Account: MariaCaiFX</div>
+          </div>
+        </div>
+        <div className='gallery-header'>
+          <img className='gallery-logo'
+               src={require('../assets/logo.png')} 
+               alt=''/>
+          <div className='gallery-desc'>Gallery</div>
         </div>
     </div>
   )
