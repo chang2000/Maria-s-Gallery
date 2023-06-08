6ebf1f8b60256a9e5309d934fa306076de7b4726
WANG Tianchang Alex <wangtcalex@gmail.com>
2023-06-08 12:37:46 -0700

    feat: add footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './Footer.css'
+import FooterHeadlineImg from '../assets/header-logo.png'
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div className="footer-headline">
+                <div className="footer-text">
+                    Contact
+                </div>
+                <img className='footer-headline-img'
+                    src={FooterHeadlineImg}
+                    alt='' />
+            </div>
+            <div className="footer-info-text">
+                Email: xxxxx@gmail.com
+            </div>
+            <div className="footer-info-text">
+                Wechat:
+            </div>
+            <div className="footer-info-text">
+                Wechat Official Account:
+            </div>
+
+        </div>
     )
 }
 
